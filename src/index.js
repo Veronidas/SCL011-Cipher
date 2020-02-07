22d2f670bab09a1fa866308657512f88c02b9690
veronidas <veronabrkich@gmail.com>
2020-02-06 23:27:24 -0300

    add function encode + decode

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-/*Acá va tu código*/
+//variables
+
+const textToCode = document.getElementById("decodeText");
+const shiftInput = document.getElementById("shift");
+const encodeB = document.getElementById("encode");
+// const decodeB = document.getElementById("decode");
+
+// encode
+encodeB.addEventListener("click", () => {
+    let textToCodeValue = textToCode.value;
+    let shiftInputValue = shiftInput.value;
+    let result = document.getElementById("result")
+    document.getElementById("result").innerHTML = "";
+    let finalResult = cipher.encode(textToCodeValue, shiftInputValue);
+    result.innerHTML = finalResult;
+});
