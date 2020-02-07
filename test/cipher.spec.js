e82b78df6419764170b060565a4fa96784f782d4
veronidas <veronabrkich@gmail.com>
2020-02-06 23:28:46 -0300

    testing of JS

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -1,25 +1,33 @@
-describe('cipher', () => {
-
-  it('debería ser un objeto', () => {
-    assert.equal(typeof cipher, 'object');
+describe("cipher.encode", () => {
+  it("debería ser un objeto", () => {
+    assert.equal(typeof cipher, "object");
   });
 
-  describe('cipher.encode', () => {
-
-    it('debería ser una función', () => {
-      assert.equal(typeof cipher.encode, 'function');
+  describe("cipher.encode", () => {
+    it("debería ser una función", () => {
+      assert.equal(typeof cipher.encode, "function");
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33');
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
+      const result = "HIJKLMNOPQRSTUVWXYZABCDEFG";
+      assert.equal(
+        window.cipher.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 33),
+        result
+      );
+    });
   });
 
-  describe('cipher.decode', () => {
-
-    it('debería ser una función', () => {
-      assert.equal(typeof cipher.decode, 'function');
+  describe("cipher.decode", () => {
+    it("debería ser una función", () => {
+      assert.equal(typeof cipher.decode, "function");
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33');
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () => {
+      const result = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      assert.equal(
+        window.cipher.decode("HIJKLMNOPQRSTUVWXYZABCDEFG", 33),
+        result
+      );
+    });
   });
-
 });
